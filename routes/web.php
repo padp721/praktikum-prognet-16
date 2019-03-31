cde8e572836a6084edd70a82852170907adfb11d
randomize721 <anggadp91@gmail.com>
2019-03-31 14:49:49 +0800

    Shop (User Side) Template Applied

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-Route::get('/', function (){
-    return view('welcome');
-});
+// Route::get('/', function (){
+//     return view('welcome');
+// });
 
+Route::get('/','ShopController@index')->name('index');
 
 Auth::routes();
 
